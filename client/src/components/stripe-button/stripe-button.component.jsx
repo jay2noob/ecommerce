80ccea243881d28eb88d456f75074af686fceabb
John Fields <jjfields24@outlook.com>
2020-05-26 23:12:15 -0400

    added stripe button and favicon

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+import "./stripe-button.styles.scss";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_rcGKk6hk9vjBY27Dc8qdP0dj00S6Zd2Clw";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="JCommerce Ltd."
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your price is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
