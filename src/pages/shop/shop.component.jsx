7370cf17b8f50c8fc2a7c6e52f8118f9998e8cda
John Fields <jjfields24@outlook.com>
2020-06-04 22:50:45 -0400

    create collectionRef

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,12 +4,21 @@ import { Route } from "react-router-dom";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
-import { firestore } from "../../firebase/firebase.utils";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
 
 class ShopPage extends React.Component {
   unsubscribeFromSnapshot = null;
 
-  componentDidMount() {}
+  componentDidMount() {
+    const collectionRef = firestore.collection("collections");
+
+    collectionRef.onSnapshot(async (snapshot) => {
+      convertCollectionsSnapshotToMap(snapshot);
+    });
+  }
 
   render() {
     const { match } = this.props;
