4b4374eeb151a2300767bd96b57e94a5e418b9da
John Fields <jjfields24@outlook.com>
2020-06-20 00:07:24 -0400

    removed unused code

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { auth } from "../../firebase/firebase.utils";
+//import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
 import { signOutStart } from "../../redux/user/user.actions";
 
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { ReactComponent as Logo } from "../../assets/original.svg";
 
 import {
   HeaderContainer,
