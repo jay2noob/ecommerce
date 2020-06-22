0d02cf0e4a64681e667b36812cdeb5e26bae500e
John Fields <jjfields24@outlook.com>
2020-06-22 17:51:06 -0400

    added onToken function to stripe-button component to access server from front end

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 import logo from "../../assets/original.svg";
 
@@ -8,8 +9,23 @@ const StripeCheckoutButton = ({ price }) => {
   const publishableKey = "pk_test_rcGKk6hk9vjBY27Dc8qdP0dj00S6Zd2Clw";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("succesful payment");
+      })
+      .catch((error) => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
   };
 
   return (
@@ -19,7 +35,7 @@ const StripeCheckoutButton = ({ price }) => {
       billingAddress
       shippingAddress
       image={logo} //"https://sendeyo.com/up/d/f3eb2117da"
-      description={`Your price is $${price}`}
+      description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
       token={onToken}
