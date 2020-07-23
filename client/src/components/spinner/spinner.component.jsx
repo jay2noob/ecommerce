71e1ee3c53ad41f90be7078784696421545c31bb
John Fields <jjfields24@outlook.com>
2020-07-23 07:49:16 -0400

    removed extension from import

--- client/src/components/spinner/spinner.component.jsx
+++ client/src/components/spinner/spinner.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { SpinnerContainer, SpinnerOverlay } from "./spinner.styles.jsx";
+import { SpinnerContainer, SpinnerOverlay } from "./spinner.styles";
 
 const Spinner = () => (
   <SpinnerOverlay>
